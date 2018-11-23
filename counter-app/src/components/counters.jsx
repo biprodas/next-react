cc0a0a18df26e3137deda80c46f1055bef6958c8
Biprodas <biprodas.cse@gmail.com>
2018-11-23 18:09:11 +0600

    added navbar

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
+    /*
     state = { 
         counters : [
             {id : 1, value : 4},
@@ -33,16 +34,17 @@ class Counters extends Component {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({counters});
     };
+    */
 
     render() { 
         return ( 
             <div>
-                <button onClick={this.handleReset} className="button btn btn-primary btn-sm m-2">Reset</button>
-                {this.state.counters.map(counter => (
+                <button onClick={this.props.onReset} className="button btn btn-primary btn-sm m-2">Reset</button>
+                {this.props.counters.map(counter => (
                     <Counter 
                         key={counter.id} 
-                        onDelete={this.handelDelete} 
-                        onIncrement={this.handleIncrement}
+                        onDelete={this.props.onDelete} 
+                        onIncrement={this.props.onIncrement}
                         counter={counter}
                     />
                 ))}
--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
+/*
 class NavBar extends Component {
     render() { 
         return ( 
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">
+                    Navbar {" "}
+                    <span className="badge badge-pill badge-secondary">
+                        {props.totalCounters}
+                    </span>
+                </a>
             </nav> 
         );
     }
 }
+ 
+export default NavBar;
+*/
+
+//Stateless Functional Component
+const NavBar = ({totalCounters}) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar {" "}
+                <span className="badge badge-pill badge-secondary">
+                    {totalCounters}
+                </span>
+            </a>
+        </nav> 
+    );
+};
  
 export default NavBar;
